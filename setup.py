e2cf50cae21c9c08b8851a34c96d8dfc3a387a9f
Billal-MOKHTARI <Billal.MOKHTARI@etu.uca.fr>
2024-04-02 21:47:06 +0200

    Update Python version classifiers

--- setup.py
+++ setup.py
@@ -16,12 +16,8 @@ setup(
         # Add any other dependencies here
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
 
